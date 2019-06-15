e6fad7ddb49b3e0567b367762f59f061e9e618de
Liad Paskin <liadpaskin@gmail.com>
2019-06-15 13:38:49 +0200

    First paralelization

--- NavierStokes_Liad2019.f90
+++ NavierStokes_Liad2019.f90
@@ -2,6 +2,8 @@
       PROGRAM FEMSIMULATION
 !    ---------------
 
+      include 'mpif.h'
+
 !
 ! **********************************************************************
 ! *                                    *
@@ -11,20 +13,32 @@
 ! **********************************************************************
 ! COPPE-PEC/LAMCE/UFRJ/2011 JLDA/KADU/IC'S
 
-    PRINT *,'hello world'
+    PRINT *,'hello world MPI'
 
-    call iomngr (0)
+    call mpi_init(ierr)
+    call mpi_comm_rank(mpi_comm_world,myid,ierr)
+    call mpi_comm_size(mpi_comm_world,numprocs,ierr)
 
-    call contrl
+      i_root = 0
+      l_root = .false.
+      if(myid .eq. i_root) l_root = .true.
 
-    CALL ALLOC (1)
+      l_debug = .false.
+      if(idebug .eq. 1) l_debug = .true.
 
-    call inmesh
+      ts_mpi = mpi_wtime()
 
-    call loads
+    !Serial IO input
+    if l_root then
+        call iomngr (0)
+        call contrl
+        CALL ALLOC (1)
+        call inmesh
+        call loads
+    endif
+    call MPI_Barrier(MPI_Comm comm)
 
     call ALLOC(3)
-
     call solver
 
     ! call output
@@ -33,6 +47,13 @@
 
     !call copyright
 
+      te_mpi = mpi_wtime()
+      write(6,9997) (te_mpi - ts_mpi)
+      write(nprt,9997) (te_mpi - ts_mpi)
+ 9997 format(' Job Execution Time = ',e15.6)
     write (*,*)' FEMSIMULATION - END PROGRAM  '
+
+    call mpi_finalize(ierr)
+
     stop
     end
